a9e898dfeb36b6863a439da56ea434a4cd43adb0
Jens N <jens@norgaard.cloud>
2023-01-22 11:19:50 +0100

    Changed logic to check Guid instead of name

--- scripts/New-PipelineWin32App.ps1
+++ scripts/New-PipelineWin32App.ps1
@@ -52,6 +52,9 @@ Write-Information -MessageData "Path: $Path"
 Write-Information -MessageData "Applications: $Application"
 [System.Array] $Applications = $Application.ToString() -split ","
 
+# Retrieve intune metadata for all current uploaded packages - eg to ensure that a package with same guid and version number is not upoaded twice
+$allWin32Apps = Get-IntuneWin32App 
+
 foreach ($App in $Applications) {
     $ApplicationName = $App.Trim()
     Write-Information -MessageData "Application: $ApplicationName"
@@ -68,11 +71,12 @@ foreach ($App in $Applications) {
     try {
         # Get existing Win32 app if present
         $DetectCurrentWin32App = $null
-        $DetectCurrentWin32App = Get-IntuneWin32App -DisplayName $($Manifest.Application.Title) |  Select -First 1
-        #Retrieve App metadata from Evergreen
+        $DetectCurrentWin32App = $allWin32Apps | Where-Object{($_.notes | ConvertFrom-Json).Guid -eq $Manifest.Information.PSPackageFactoryGuid}
+
+        # Retrieve App metadata from Evergreen
         $AppData = Invoke-Expression -Command $Manifest.Application.Filter
-        #Exit if Win32 app version already exists
-        $NewVersion = ($DetectCurrentWin32App.displayVersion -ne $AppData.Version)
+        # NewVersion identified (true/false)
+        $NewVersion = ($($DetectCurrentWin32App.displayVersion | Sort-Object -Descending |  Select-Object -First 1) -ne $AppData.Version)
     }
     catch {
         throw $_
