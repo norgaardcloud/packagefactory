3280b7b829b8ad9cabea602ad4c5dc437777d9a6
norjen <jens@norgaard.cloud>
2023-01-27 16:51:12 +0100

    revert

--- scripts/New-PipelineWin32App.ps1
+++ scripts/New-PipelineWin32App.ps1
@@ -112,12 +112,7 @@ foreach ($App in $Applications) {
         else {
 
             # Get the application installer via Evergreen and download
-            if($ApplicationName -eq "7zip"){
-                $result = Invoke-Expression -Command $Manifest.Application.Filter | Save-EvergreenApp -CustomPath $([System.IO.Path]::Combine($Path, $PackageFolder, $Type, $ApplicationName, $Manifest.PackageInformation.SourceFolder)) -UserAgent 'Googlebot/2.1 (+http://www.google.com/bot.html)'
-            }
-            else{
-                $result = Invoke-Expression -Command $Manifest.Application.Filter | Save-EvergreenApp -CustomPath $([System.IO.Path]::Combine($Path, $PackageFolder, $Type, $ApplicationName, $Manifest.PackageInformation.SourceFolder))
-            }
+            $result = Invoke-Expression -Command $Manifest.Application.Filter | Save-EvergreenApp -CustomPath $([System.IO.Path]::Combine($Path, $PackageFolder, $Type, $ApplicationName, $Manifest.PackageInformation.SourceFolder))
             
 
             # Unpack the installer file if its a zip file
